2a2fd47773d534739dc2731c6daffe443800b048
Sharon Zavala <sharon@macbook-pro.gateway.pace.com>
2017-05-27 10:28:34 -0700

    js done, but will revert and stash

--- testFolder/test.js
+++ testFolder/test.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 
-var url = window.locations.search;
-
-
-var postArr = [];
+//var url = window.locations.search;
 
 
 
+//sidebar container holding all of the posts
+var postContainer = $("#postContainer");
 
-//updating post to push out older posts
+//logic for movement of post to push out older posts
 tlPost = new TimelineLite();
 
 	el = $(".mdl-card");
@@ -28,6 +27,9 @@ tlPost = new TimelineLite();
 //grab post to append to array 
 var userPost = $("#body");
 var form = $("postForm");
+//empty variables
+var postArr;
+var posts;
 
 $(form).on("#submitPost", function handleFormSubmit(event){
 	event.preventDefault();
@@ -40,6 +42,8 @@ $(form).on("#submitPost", function handleFormSubmit(event){
 		body: userPost.val().trim()
 	}
 
+	var postArr = [];
+
 	console.log(newPost);
 
 	postArr.push(newPost);
@@ -47,19 +51,83 @@ $(form).on("#submitPost", function handleFormSubmit(event){
 });
 
 
-//grab click listeners on friend posts 
-
-var postContainer = $("#postContainer");
 
 
+
+//grab posts from database
 function getPosts(sidebar){
-	$.get("/", function)
+	$.get("/", function(data){
+		console.log(data);
+
+		initializePosts();
+	});
+
+}
+//getting the inital list of posts
+getPosts();
+
+// InitializePosts handles appending all of our constructed post HTML inside
+// postContainer
+function initializePosts(){
+	postContainer.empty();
+	var postArr = [];
+
+	for (var i = 0; i < posts.length; i++) {
+		postArr.push(newPostCreated(posts[i]);
+	}
+	postContainer.append(postArr);
+}
+
+function newPostCreated(post) {
+	var newPostPanel = $("<div>");
+	newPostPanel.addClass("mdl-card mdl-cell mdl-cell--12-col mdl-cell--2-col-tablet mdl-shadow--2dp");
+	var newPostPanelHeading = $("<div>");
+	newPostPanelHeading.addClass("mdl-card__title");
+	var newPostPanelTitle = $("<div>");
+	newPostPanelTitle.addClass("mdl-card__title-text");
+	var titleH2 = $("<h2>");
+	titleH2.addClass("mdl-card__title-text");
+>>>	titleH2.text({{{Username, timestamp}}});
+	var newPostPanelBody = $("<div>");
+	newPostPanelBody.addClass("mdl-card__supporting-text");
+	var newPostBody = $("<p>");
+>>>	newPostBody.text({{{post}}});
+	var newPostActionBorder = $("<div>");
+	newPostActionBorder.addClass("mdl-card__actions mdl-card--border");
+	var newPostActionBtn = $("<a>");
+	newPostActionBtn.addClass("mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect");
+	newPostActionBtn.text("Visit Profile");
+	var newPostPanelSpacer = $("<div>");
+	newPostPanelSpacer.addClass("mdl-layout-spacer");
+	var newPostFaviconBtn = $("<button>");
+	newPostFaviconBtn.addClass("mdl-button mdl-button--icon mdl-button--colored");
+	var postFavicon = $("<i>");
+	postFavicon.addClass("material-icons");
+	postFavicon.text("favorite");
+	var newPostShareBtn = $("<button>");
+	newPostShareBtn.addClass("mdl-button mdl-button--icon mdl-button--colored");
+	var shareFavicon = $("<i>");
+	shareFavicon.addClass("material-icons");
+	shareFavicon.text("share");
+
+	newPostPanelTitle.append(titleH2);
+	newPostPanelHeading.append(newPostPanelTitle);
+	newPostPanelBody.append(newPostBody);
+	newPostPanel.append(newPostPanelHeading);
+	newPostPanel.append(newPostPanelBody);
+
+	newPostPanel.data(posts)
+	return newPostPanel;
 
 }
 
 
 
+//grab click listeners on friend posts 
+
+
 });
 
-	//var progress = myAnimation.progress(); //gets current progress
-//myAnimation.progress(0.25); //sets progress to one quarter finished
+
+
+	
